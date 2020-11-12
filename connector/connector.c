c4db945e2a704efc6cd85cfb426136cb129ebd12
believening <kang_417@yeah.net>
2020-11-13 03:24:43 +0800

    debug for connector
    Signed-off-by: believening <kang_417@yeah.net>

--- connector/connector.c
+++ connector/connector.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
 #include <linux/cn_proc.h>
 #include <linux/connector.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define CNMSG_LEN sizeof(struct cn_msg)
 #define CNMCASTOP_LEN sizeof(enum proc_cn_mcast_op)
+#define PROCEVENT_LEN sizeof(struct proc_event)
 
 int seq = 0;
 
-void change_cn_proc_mode(int mode)
-{
-    struct nlmsghdr *nlhdr = NULL;
-    memset(nlhdr, 0, NLMSG_HDRLEN + sizeof(int));
-}
-
 // 创建连接
-int connect()
+static int create_connect()
 {
     int s; // socket fd
     struct sockaddr_nl l_local;
@@ -34,7 +30,7 @@ int connect()
 
     // bind
     l_local.nl_family = AF_NETLINK;
-    l_local.nl_groups = 0;
+    l_local.nl_groups = 1;
     l_local.nl_pid = getpid();
 
     if (bind(s, (struct sockaddr *)&l_local, sizeof(l_local)) != 0)
@@ -47,56 +43,123 @@ int connect()
 }
 
 // 控制事件侦听
-int control_event_listenning(int s, enum proc_cn_mcast_op op)
+static int control_event_listenning(int sd, enum proc_cn_mcast_op op)
 {
     int ret;
-    struct nlmsghdr *hdr = NULL; // netlink 消息头
-    struct cn_msg *cn = NULL;    // netlink connector 消息
-    // struct msghdr *msg = NULL;
+    int msg_len;
+    struct nlmsghdr nlh = {}; // netlink 消息头
+    struct cn_msg cn = {};    // netlink connector 消息
     enum proc_cn_mcast_op *o;
 
-    memset(hdr, 0, NLMSG_HDRLEN);
-    hdr->nlmsg_len = NLMSG_LENGTH(CNMSG_LEN + CNMCASTOP_LEN);
-    hdr->nlmsg_type = NLMSG_DONE;
-    hdr->nlmsg_flags = 0;
-    hdr->nlmsg_seq = seq;
-    hdr->nlmsg_pid = getpid();
+    msg_len = NLMSG_SPACE(CNMSG_LEN + CNMCASTOP_LEN); // 申请对齐空间
+    memset(&nlh, 0, msg_len);
+    nlh.nlmsg_len = msg_len;
+    nlh.nlmsg_type = NLMSG_DONE;
+    nlh.nlmsg_flags = 0;
+    nlh.nlmsg_seq = seq;
+    nlh.nlmsg_pid = getpid();
 
-    cn = (struct cn_msg *)NLMSG_DATA(hdr); // 取得 netlink 消息中数据部分的首地址
-    cn->id.idx = CN_IDX_PROC;
-    cn->id.val = CN_VAL_PROC;
-    cn->len = CNMCASTOP_LEN;
-    cn->seq = seq++;
-    cn->ack = 0;
-    o = (enum proc_cn_mcast_op *)cn->data; // 取得 connector 消息中数据部分的首地址
-    *o = op;                               // 将控制模式写入到 connector 消息的数据部分中
+    memset(&cn, CNMSG_LEN, 0); // 构造 cn_msg
+    cn.id.idx = CN_IDX_PROC;
+    cn.id.val = CN_VAL_PROC;
+    cn.len = CNMCASTOP_LEN;
+    cn.seq = seq++;
+    cn.ack = 0;                           // 对于非响应消息，ack 应当设置为 0
+    o = (enum proc_cn_mcast_op *)cn.data; // 取得 connector 消息中数据部分的首地址
+    *o = op;                              // 将控制模式写入到 connector 消息的数据部分中
 
-    ret = send(s, hdr, hdr->nlmsg_len, 0);
-    if (ret != 0)
+    memcpy(NLMSG_DATA(&nlh), &cn, CNMSG_LEN + cn.len); // 填充 nlh 的 cn_msg
+
+    ret = send(sd, &nlh, nlh.nlmsg_len, 0); // 发送单个消息使用 send() 函数
+    if (ret == -1)
     {
         perror("netlink connector send");
-        return -1;
     }
     return ret;
 }
-//
+
+// 接受并处理事件
+static int handle_received_event(int sd)
+{
+    int ret;
+    int msg_len;
+    struct nlmsghdr hdr = {};
+    struct cn_msg cn = {};
+    struct proc_event *e;
+
+    msg_len = NLMSG_SPACE(CNMSG_LEN + PROCEVENT_LEN);
+    // memset(&hdr, 0, msg_len);
+
+    ret = recv(sd, &hdr, msg_len, 0);
+    if (ret == 0) // 接收了 0 个字节的内容
+    {
+        return 0;
+    }
+    else if (ret == -1)
+    {
+        perror("recv");
+        return -1;
+    }
+
+    // memset(&cn, 0, CNMSG_LEN + PROCEVENT_LEN);
+    memcpy(&cn, NLMSG_DATA(&hdr), ret - NLMSG_HDRLEN);
+    e = (struct proc_event *)cn.data;
+    switch (e->what)
+    {
+    case PROC_EVENT_EXEC:
+        printf("process event:\texec\t[time:%lld,pid:%d,tgid:%d]\n",
+               e->timestamp_ns,
+               e->event_data.exec.process_pid,
+               e->event_data.exec.process_tgid);
+        break;
+    case PROC_EVENT_EXIT:
+        printf("process event:\texit\t[time:%lld,pid:%d,tgid:%d,exit:%d]\n",
+               e->timestamp_ns,
+               e->event_data.exit.process_pid,
+               e->event_data.exit.process_tgid,
+               e->event_data.exit.exit_code);
+        break;
+    default:
+        printf("process event: \t%d\n", e->what);
+        break;
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
-    int s; // socket fd
-    struct sockaddr_nl l_local;
+    int sd; // socket fd
+    int ret;
 
     // connect
-    s = connect();
-    if (s == -1)
+    sd = create_connect();
+    if (sd == -1)
     {
         perror("connect");
         return -1;
     }
 
     // register listenning
-    if (control_event_listenning(s, PROC_CN_MCAST_LISTEN) != 0)
+    if (control_event_listenning(sd, PROC_CN_MCAST_LISTEN) == -1)
     {
-        perror("listen listenning");
-        return -1;
+        perror("register listen listenning");
+        ret = -1;
+        goto out;
     }
+
+    // handler event
+    while (1)
+    {
+        ret = handle_received_event(sd);
+        if (ret == -1)
+        {
+            perror("handle received event");
+            ret = -1;
+            goto out;
+        }
+    }
+
+out:
+    close(sd);
+    return ret;
 }
--- connector/readme.md
+++ connector/readme.md
@@ -46,10 +46,10 @@ struct cn_msg {
 
 需要注意的是 `nlmsghdr.nlmsg_len` 所指的长度是包含了`nlmsghdr`自身、`cn_msg`、以及消息内容本身的字节长度。 而 `cn_msg.len` 仅仅指的是消息内容本身的长度。
 
-
 ## 参考链接
-* [connector](https://www.kernel.org/doc/Documentation/*nnector/connector.txt)
-* [connect.h](https://code.woboq.org/linux/linux/include/linux/*nnector.h.html)
-* [cn_proc.h](https://code.woboq.org/linux/linux/include/linux/*_proc.h.html)
-* [连接器（Netlink Connector）及其应用](https://blog.csdn.net/*nat_32343037/article/details/78487272)
+
+* [connector](https://www.kernel.org/doc/Documentation/connector/connector.txt)
+* [connect.h](https://code.woboq.org/linux/linux/include/linux/connector.h.html)
+* [cn_proc.h](https://code.woboq.org/linux/linux/include/linux/cn_proc.h.html)
+* [连接器（Netlink Connector）及其应用](https://www.ibm.com/developerworks/cn/linux/l-connector/)
 * [linux process monitoring](https://bewareofgeek.livejournal.com/2945.html)
