e3676902cc2b1bc79e6e0dc79d3b0d4b1a81f1d5
believening <kang_417@yeah.net>
2020-12-08 20:34:08 +0800

    update go monitor
    Signed-off-by: believening <kang_417@yeah.net>

--- connector/connector.go
+++ connector/connector.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+	"os"
+	"syscall"
+	"unsafe"
+
+	"golang.org/x/sys/unix"
+)
+
 const (
 	// #define CN_IDX_PROC                     0x1
 	cnIdxProc uint32 = 0x1
@@ -14,6 +23,16 @@ const (
 	PROC_CN_MCAST_IGNORE = 2
 )
 
+var (
+	seq uint32 = 0
+
+	sa = &unix.SockaddrNetlink{
+		Family: unix.AF_NETLINK,
+		Groups: cnIdxProc,
+		Pid:    uint32(os.Getpid()),
+	}
+)
+
 // map c struct cb_id defined in connector.h
 type cbID struct {
 	idx uint32
@@ -22,13 +41,68 @@ type cbID struct {
 
 // map c struct cn_msg defined in connector.h
 type cnMsg struct {
-	id   cbID
-	seq  uint32
-	ack  uint32
-	len  uint32
-	data int8
+	id    cbID
+	seq   uint32
+	ack   uint32
+	len   uint16
+	flags uint16
+	// data  int8 该处为　proc msg 地址
+}
+
+type nlSocket struct {
+	fd     int
+	binded bool
+}
+
+func (nl *nlSocket) Connect() error {
+	err := unix.Bind(nl.fd, sa)
+	nl.binded = (err == nil)
+	return err
+}
+
+// func (nl *nlSocket) enableMonitor(en bool) {
+// 	cnmsg := cnMsg{
+// 		id: cbID{cnIdxProc, cnValProc},
+// 	}
+// 	nlh := unix.NlMsghdr{
+// 		Len:   uint32(unix.NLMSG_HDRLEN) + (uint32)(unsafe.Sizeof(cnmsg)),
+// 		Type:  unix.NLMSG_DONE,
+// 		Flags: 0, // /usr/include/linux/netlink.h
+// 		Pid:   uint32(os.Getpid()),
+// 		Seq:   seq,
+// 	}
+// 	unix.Sendto(nl.fd, []byte{}, 0, sa)
+// }
+
+func getSocket() (*nlSocket, error) {
+	sock, err := syscall.Socket(syscall.AF_NETLINK, syscall.SOCK_DGRAM, syscall.NETLINK_CONNECTOR)
+	if err != nil {
+		return nil, err
+	}
+	return &nlSocket{sock, false}, nil
 }
 
 func main() {
+	nl, err := getSocket()
+	if err != nil {
+		log.Fatalf("socket: %v\n", err)
+	}
+	if err := nl.Connect(); err != nil {
+		log.Fatalf("bind: %v\n", err)
+	}
 
+	cnmsg := cnMsg{
+		id:  cbID{cnIdxProc, cnValProc},
+		seq: seq,
+		ack: 0,
+		len: 1,
+	}
+	nlh := unix.NlMsghdr{
+		Len:   uint32(unix.NLMSG_HDRLEN) + (uint32)(unsafe.Sizeof(cnmsg)),
+		Type:  unix.NLMSG_DONE,
+		Flags: 0, // /usr/include/linux/netlink.h
+		Pid:   uint32(os.Getpid()),
+		Seq:   seq,
+	}
+	unix.Sendto(nl.fd, []byte{}, 0, sa)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/believening/netlink-monitor
 
 go 1.13
 
-require golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634
+require (
+	github.com/go-playground/validator/v10 v10.4.1
+	golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634
+)
